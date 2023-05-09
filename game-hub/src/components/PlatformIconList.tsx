0b0924b2dbb2cfd51325cf7fe1df81a7288d3e9e
Dustin Yang <srdpt3@gmail.com>
2023-05-08 20:59:24 -0400

    update

--- game-hub/src/components/PlatformIconList.tsx
+++ game-hub/src/components/PlatformIconList.tsx
@@ -1,11 +1,41 @@
 import React from "react";
-
+import { Platform } from "../hooks/userGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneAndroid, MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
 interface Props {
   platform: Platform[];
 }
 
-const PlatformIconList = () => {
-  return <div>PlatformIconList</div>;
+const PlatformIconList = ({ platform }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playStation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+    web: BsGlobe,
+  };
+  return (
+    <HStack marginY={1}>
+      {platform.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500"></Icon>
+      ))}
+    </HStack>
+  );
 };
 
 export default PlatformIconList;
--- game-hub/src/hooks/userGames.ts
+++ game-hub/src/hooks/userGames.ts
@@ -16,6 +16,7 @@ export interface Game {
     name: string;
     background_image: string;
     parent_platforms: { platform: Platform }[];
+    metacritic: number;
 }
 
 interface FetchGamesResponse {
