f54c289c01283e95c3c36c3d366c3fefd2dbe7d5
Dustin Yang <srdpt3@gmail.com>
2023-05-11 21:08:37 -0400

    update

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -10,13 +10,18 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        {/* <GridItem area="aside">Aside</GridItem> */}
-        <GenreList></GenreList>
+        <GridItem area="aside" paddingX={5}>
+          <GenreList></GenreList>
+        </GridItem>
       </Show>
 
       <GridItem area="main">
