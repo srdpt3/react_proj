0d2492f32b151b9d30b6d110168f6e0b87fb7c60
Dustin Yang <srdpt3@gmail.com>
2023-05-13 10:38:17 -0400

    clean up game card

--- game-hub/src/components/Gamecard.tsx
+++ game-hub/src/components/Gamecard.tsx
@@ -15,13 +15,13 @@ const Gamecard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platform={game.parent_platforms.map((p) => p.platform)}
           ></PlatformIconList>
           <CriticScore score={game.metacritic}></CriticScore>
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
