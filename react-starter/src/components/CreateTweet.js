c80797e81998120a3c8d185a6d95cbf4fdbae98c
Dustin Yang <srdpt3@gmail.com>
2023-05-15 21:46:51 -0400

    react test

---
+++ react-starter/src/components/CreateTweet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CreateTweet = () => {
+  return (
+    <form>
+      <textarea cols="30" rows="10"></textarea>
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default CreateTweet;
---
+++ react-starter/src/components/Tweet.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tweet = ({ name }) => {
+  return <h2>{name}</h2>;
+};
+
+export default Tweet;
