75bc0b51c61e678f4616a9871eefaa002bdba605
Dustin Yang <srdpt3@gmail.com>
2023-05-21 07:21:21 -0400

    started new project capture_portfolio

--- music-player/src/components/Nav.js
+++ music-player/src/components/Nav.js
@@ -4,7 +4,7 @@ import { faMusic } from "@fortawesome/free-solid-svg-icons";
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
   return (
     <nav>
-      <h1>Wwaves</h1>
+      <h1>DS Music</h1>
       <button onClick={() => setLibraryStatus(!libraryStatus)}>
         Library <FontAwesomeIcon icon={faMusic}></FontAwesomeIcon>
       </button>
