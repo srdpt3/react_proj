d68f9121fea7b6fc403f767a72564807a9b25407
Dustin Yang <srdpt3@gmail.com>
2023-05-21 17:18:21 -0400

    service section + faq section

---
+++ capture_portpolio/src/components/FaqSection.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+            autem accusamus ex laboriosam porro, adipisci quam voluptatum magnam
+            placeat corporis.
+          </p>
+        </div>
+      </div>
+
+      <div className="Daily Schedule">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+            autem accusamus ex laboriosam porro, adipisci quam voluptatum magnam
+            placeat corporis.
+          </p>
+        </div>
+      </div>
+
+      <div className="Diferrent Payment Methods">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+            autem accusamus ex laboriosam porro, adipisci quam voluptatum magnam
+            placeat corporis.
+          </p>
+        </div>
+      </div>
+
+      <div className="What Products do you offer.">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+            autem accusamus ex laboriosam porro, adipisci quam voluptatum magnam
+            placeat corporis.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- capture_portpolio/src/pages/AboutUs.js
+++ capture_portpolio/src/pages/AboutUs.js
@@ -1,8 +1,16 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServiceSection from "../components/ServiceSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServiceSection></ServiceSection>
+      <FaqSection></FaqSection>
+    </>
+  );
 };
 
 export default AboutUs;
