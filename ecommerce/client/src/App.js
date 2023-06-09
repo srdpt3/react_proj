f182bcfe6519d228d7727e8bdf08939f61fc77f9
Dustin Yang <srdpt3@gmail.com>
2023-06-09 06:26:23 -0400

    reducer setup

---
+++ ecommerce/client/src/App.js
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./scenes/home/Home";
+import ItemDetail from "./scenes/itemDetails/ItemDetail";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+import NavBar from "./scenes/global/NavBar";
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    console.log(pathname);
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <ScrollToTop></ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="item:itemId" element={<ItemDetail />}></Route>
+          <Route path="checkout" element={<Checkout />}></Route>
+          <Route path="checkout/succuess" element={<Confirmation />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ ecommerce/client/src/scenes/global/NavBar.jsx
@@ -0,0 +1,70 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Badge, Box, IconButton } from "@mui/material";
+import {
+  PersonOutline,
+  ShoppingBagOutlined,
+  MenuOutlined,
+  SearchOutlined,
+  MenuOpenOutlined,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { shades } from "../../theme";
+// import { setIsCartOpen } from "../../state";
+
+const NavBar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      width="100%"
+      height="60px"
+      backgroundColor="rgba(255,255,255,0.95)"
+      color="black"
+      postion="fixed"
+      top="0"
+      left="0"
+      zIndex="1"
+    >
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        alignContent="center"
+      >
+        <Box
+          onClick={() => navigate("/")}
+          sx={{ "&:hover": { cursor: "pointer" } }}
+          color={shades.secondary[500]}
+        >
+          ECOMMER
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          columnGap="20px"
+          zIndex="2"
+        >
+          <IconButton sx={{ color: "black" }}>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton sx={{ color: "black" }}>
+            <PersonOutline />
+          </IconButton>
+
+          <IconButton sx={{ color: "black" }}>
+            <ShoppingBagOutlined />
+          </IconButton>
+
+          <IconButton sx={{ color: "black" }}>
+            <MenuOpenOutlined />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default NavBar;
