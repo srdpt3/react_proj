7b857b91114e5c4482677d4c2f677d6aefe9cf80
Dustin Yang <srdpt3@gmail.com>
2023-07-01 23:26:24 -0400

    upload images

--- airbnb-react-vite/api/index.js
+++ airbnb-react-vite/api/index.js
@@ -6,11 +6,17 @@ const cors = require("cors");
 const User = require("./models/User");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const download = require("image-downloader");
+const fs = require("fs");
+
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = "asdfaskjfklas;djfkasjejkd";
+app.use(express.json());
+app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
 app.use(
   cors({
     credentials: true,
@@ -82,6 +88,31 @@ app.get("/profile", async (req, res) => {
 });
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json(true);
+});
+
+app.post("/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = "photo_" + Date.now() + ".jpg";
+  await download.image({
+    url: link,
+    dest: __dirname + "/uploads/" + newName,
+  });
+  res.json(newName);
+});
+
+const photoMiddleware = multer({ dest: "uploads" });
+app.post("/upload", photoMiddleware.array("photos", 100), async (req, res) => {
+  const uploadedfiles = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const { path, originalname } = req.files[i];
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+
+    const newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+    uploadedfiles.push(newPath.replace("uploads/", ""));
+  }
+  res.json(uploadedfiles);
 });
 
 app.listen(4000);
